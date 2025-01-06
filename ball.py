eabb2404e2943f4fe32067b5cd4171a4c8f66b89
ChenszHuang <Chensz.hengki@gmail.com>
2025-01-06 22:23:36 +0700

    make the ball goes random direction after pass over and speed increment

--- ball.py
+++ ball.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+import random
 
 
 class Ball(Turtle):
@@ -9,6 +10,8 @@ class Ball(Turtle):
         self.penup()
         self.x_step = 10
         self.y_step = 10
+        self.step = [-1, 1]
+        self.ball_speed = 0.04
 
     def move(self):
         new_x = self.xcor() + self.x_step
@@ -20,3 +23,11 @@ class Ball(Turtle):
 
     def bounce_x(self):
         self.x_step *= -1
+        if self.ball_speed - 0.002 != 0:
+            self.ball_speed -= 0.002
+
+    def respawn_ball(self):
+        self.goto(0, 0)
+        self.ball_speed = 0.04
+        self.x_step *= random.choice(self.step)
+        self.y_step *= random.choice(self.step)
