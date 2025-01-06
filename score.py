fb20ee0cfc20cfb6a4a8d43f5ca5f8458ee04c94
ChenszHuang <chensz.hengki@gmail.com>
2025-01-06 14:19:14 +0700

    created a score system for scoring in pong using turtle

--- score.py
+++ score.py
@@ -1,1 +1,24 @@
-#bikin score disini dengan tampilan 0 | 0
+from turtle import Turtle
+
+
+class Score(Turtle):
+    ALIGN = "center"
+    FONT = ("Arial", 40, "bold")
+
+    def __init__(self):
+        super().__init__()
+        self.left_score = 0
+        self.right_score = 0
+        self.penup()
+        self.color("white")
+        self.hideturtle()
+        self.goto(self.xcor(), 220)
+        self.count_score()
+
+    def count_score(self):
+        self.clear()
+        self.write(
+            f"{self.left_score} | {self.right_score}",
+            align=Score.ALIGN,
+            font=Score.FONT,
+        )
